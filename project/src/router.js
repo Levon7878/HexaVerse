ec241f56f6c68543d4e4361017c169c0586b4ea9
Levon7878 <lyovbabagulyan@gmail.com>
2025-08-26 02:09:45 +0400

    Fix router + add _redirects

--- project/src/router.js
+++ project/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home/Home.vue";
 import DigitalMarketing from "@/pages/DigitalMarketing/DigitalMarketing.vue";
 import DigitalPiar from "@/pages/DigitalPR/DigitalPiar.vue";
@@ -13,34 +13,39 @@ import ClientTest from "@/pages/Client/ClientTest.vue";
 import Contact from "@/pages/Contact/Contact.vue";
 import BlogPage from "@/pages/BlogPage/BlogPage.vue";
 import Demo from "@/pages/Demo/Demo.vue";
-import CalendarDemo from "./components/CalendarDemo.vue";
-import UseFormDemo from "./components/UseFormDemo.vue";
-import getInTouch from "./pages/GetInTouch/GetInTouch.vue";
-import Newsletter from "./pages/Newsletter/Newsletter.vue";
-export default createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    { path: "/", component: Home },
-    { path: "/home", component: Home },
-    { path: "/DigitalMarketing", component: DigitalMarketing },
-    { path: "/DigitalPiar", component: DigitalPiar },
-    { path: "/ProjectManagment", component: ProjectManagments },
-    { path: "/WebDevelopment", component: WebDevelopment },
-    { path: "/WebDesign", component: WebDesign },
-    { path: "/GraphicDesign", component: GraphicDesign },
-    { path: "/CmsDev", component: CmsDevelopment },
-    { path: "/AiDevelopment", component: AiDevelopment },
-    { path: "/Team", component: Team },
-    { path: "/Client", component: ClientTest },
-    { path: "/Contact", component: Contact },
-    { path: "/Blog", component: BlogPage },
-    { path: "/demo", component: Demo },
-    { path: "/calendar", component: CalendarDemo },
-    { path: "/formdemo", component: UseFormDemo },
-    { path: "/getInTouch", component: getInTouch },
-    { path: "/newsLetter", component: Newsletter },
-  ],
-  scrollBehavior(to, from, savedPosition) {
+import CalendarDemo from "@/components/CalendarDemo.vue";
+import UseFormDemo from "@/components/UseFormDemo.vue";
+import getInTouch from "@/pages/GetInTouch/GetInTouch.vue";
+import Newsletter from "@/pages/Newsletter/Newsletter.vue";
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/home", component: Home },
+  { path: "/DigitalMarketing", component: DigitalMarketing },
+  { path: "/DigitalPiar", component: DigitalPiar },
+  { path: "/ProjectManagment", component: ProjectManagments },
+  { path: "/WebDevelopment", component: WebDevelopment },
+  { path: "/WebDesign", component: WebDesign },
+  { path: "/GraphicDesign", component: GraphicDesign },
+  { path: "/CmsDev", component: CmsDevelopment },
+  { path: "/AiDevelopment", component: AiDevelopment },
+  { path: "/Team", component: Team },
+  { path: "/Client", component: ClientTest },
+  { path: "/Contact", component: Contact },
+  { path: "/Blog", component: BlogPage },
+  { path: "/demo", component: Demo },
+  { path: "/calendar", component: CalendarDemo },
+  { path: "/formdemo", component: UseFormDemo },
+  { path: "/getInTouch", component: getInTouch },
+  { path: "/newsLetter", component: Newsletter },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior() {
     return { top: 0 };
   },
 });
+
+export default router;
